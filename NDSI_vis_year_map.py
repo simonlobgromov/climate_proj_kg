93b34edb9fa3f93f1b2143a62ab0183d6dd2de5b
Den <131668061+simonlobgromov@users.noreply.github.com>
2024-03-07 01:26:59 +0600

    Update NDSI_vis_year_map.py

--- NDSI_vis_year_map.py
+++ NDSI_vis_year_map.py
@@ -4,22 +4,24 @@ import  pandas as pd
 import sqlite3
 import boto3
 import argparse
+import matplotlib.pyplot as plt
 
-s3 = boto3.client(
-    's3',
-    aws_access_key_id='AKIAZI2LCDTNTYZBZVFM',
-    aws_secret_access_key='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',
-    region_name='ap-northeast-1'
-)
-response = s3.list_buckets()
-buckets = [bucket['Name'] for bucket in response['Buckets']]
 
-def upload_file(file_key):
-  file_name = file_key.split('/')[-1]
-  local_file_path = f'./{file_name}'
-  s3.download_file(buckets[0], file_key, local_file_path)
+# s3 = boto3.client(
+#     's3',
+#     aws_access_key_id='AKIAZI2LCDTNTYZBZVFM',
+#     aws_secret_access_key='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',
+#     region_name='ap-northeast-1'
+# )
+# response = s3.list_buckets()
+# buckets = [bucket['Name'] for bucket in response['Buckets']]
 
-upload_file('visualization_data_sql/data_visualization_sql.db')
+# def upload_file(file_key):
+#   file_name = file_key.split('/')[-1]
+#   local_file_path = f'./{file_name}'
+#   s3.download_file(buckets[0], file_key, local_file_path)
+
+# upload_file('visualization_data_sql/data_visualization_sql.db')
 conn = sqlite3.connect('data_visualization_sql.db')
 
 parser = argparse.ArgumentParser(description='Script description')
@@ -31,7 +33,10 @@ query = f"SELECT * FROM NDSI_location_{year}"
 NDSI_location = pd.read_sql_query(query, conn)
 conn.close()
 
-fig = px.density_mapbox(NDSI_location, lat='lat', lon='lon', z='NDSI', radius=12, zoom=7, opacity=.7, color_continuous_scale='GnBu',
-                        center=dict(lat=41.931586, lon=74.582786), mapbox_style="open-street-map", animation_frame="month")
+plt.ion()
+
+fig = px.density_mapbox(NDSI_location, lat='lat', lon='lon', z='NDSI', radius=12, zoom=7, opacity=.7, color_continuous_scale='GnBu', center=dict(lat=41.931586, lon=74.582786), mapbox_style="open-street-map", animation_frame="month")
 fig.update_layout(width=1600, height=1200)
 fig.show()
+
+input("Press Enter to continue...")
